df8f59d6846da981a82a67b21794f2154a15a8cd
AyoubBoudhafr <a23mohbounij@inspedralbes.cat>
2024-12-03 10:01:31 +0100

    movimiento entre componentes mejorado

---
+++ web/src/components/ApuntsScreen.vue
@@ -0,0 +1,20 @@
+<script>
+  import { inject, onMounted } from 'vue';
+  
+  export default {
+    setup() {
+      const divActivo = inject('divActivo'); 
+  
+      onMounted(() => {
+        if (divActivo) {
+          divActivo.value = 'apunts';
+        }
+      });
+  
+      return {};
+    },
+  };
+  </script>
+  <template>
+    <h1>Aqui aniran els apunts</h1>
+  </template>
