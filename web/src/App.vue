5d1352b4e85ca4ddc3a37af516c3e9d37c6bc1c6
a19pabmatpav <a19pabmatpav@inspedralbes.cat>
2024-12-04 10:56:30 +0100

    footer añadido

--- web/src/App.vue
+++ web/src/App.vue
@@ -60,57 +60,65 @@ export default {
       </q-tabs>
     </q-header>
 
-    <q-drawer
-      v-if="divActivo === 'login' || divActivo === 'register'"
-      show-if-above
-      v-model="isLeftDrawerOpen"
-      side="left"
-      bordered
-    >
+    <q-drawer v-if="divActivo === 'login' || divActivo === 'register'" show-if-above v-model="isLeftDrawerOpen"
+      side="left" bordered>
       <q-tabs vertical>
         <q-route-tab to="/register" label="REGISTRATE" />
         <q-route-tab to="/Puntuacions" label="Puntuacións" />
         <q-route-tab to="/Configuracio" label="CONFIGURACIÓ" />
-        <q-route-tab
-          to="/Cerrar-sesion"
-          label="Tancar sessió"
-          class="text-red"
-        />
+        <q-route-tab to="/Cerrar-sesion" label="Tancar sessió" class="text-red" />
       </q-tabs>
-</q-drawer>
-<q-drawer v-if="divActivo === 'offline'"  show-if-above v-model="isLeftDrawerOpen" side="left" bordered>
-  <q-tabs vertical>
-    <q-route-tab to="/sumaOffline" label="SUMA"/>
-    <q-route-tab to="/restaOffline" label="RESTA"/>
-    <q-route-tab to="/multiplicacionOffline" label="MULTIPLICACION"/>
-    <q-route-tab to="/divisionOffline" label="DIVISION"/>
-  </q-tabs>
-</q-drawer>
-<q-drawer v-if="divActivo === 'apunts'"  show-if-above v-model="isLeftDrawerOpen" side="left" bordered>
-  <q-tabs vertical>
-    <q-route-tab to="/tablasMultiplicar" label="TAULES DE MULTIPLICAR"/>
-    <q-route-tab to="/equacions" label="EQUACIONS"/>
-    <q-route-tab to="/pitagoras" label="PITAGORAS"/>
-  </q-tabs>
-</q-drawer>
-<q-drawer v-if="divActivo === 'jocs'"  show-if-above v-model="isLeftDrawerOpen" side="left" bordered>
-  <q-tabs vertical>
-    <q-route-tab to="/ocaMatematica" label="Oca Matematica"/>
-    <q-route-tab to="/buscaminas" label="BUSCAMINAS"/>
-  </q-tabs>
-</q-drawer>
-<q-drawer v-if="divActivo === 'online'"  show-if-above v-model="isLeftDrawerOpen" side="left" bordered>
-  <q-tabs vertical>
-    <q-route-tab to="/sumaOffline" label="SUMA"/>
-    <q-route-tab to="/restaOffline" label="RESTA"/>
-    <q-route-tab to="/multiplicacionOffline" label="MULTIPLICACION"/>
-    <q-route-tab to="/divisionOffline" label="DIVISION"/>
-  </q-tabs>
+    </q-drawer>
+    <q-drawer v-if="divActivo === 'offline'" show-if-above v-model="isLeftDrawerOpen" side="left" bordered>
+      <q-tabs vertical>
+        <q-route-tab to="/sumaOffline" label="SUMA" />
+        <q-route-tab to="/restaOffline" label="RESTA" />
+        <q-route-tab to="/multiplicacionOffline" label="MULTIPLICACION" />
+        <q-route-tab to="/divisionOffline" label="DIVISION" />
+      </q-tabs>
+    </q-drawer>
+    <q-drawer v-if="divActivo === 'apunts'" show-if-above v-model="isLeftDrawerOpen" side="left" bordered>
+      <q-tabs vertical>
+        <q-route-tab to="/tablasMultiplicar" label="TAULES DE MULTIPLICAR" />
+        <q-route-tab to="/equacions" label="EQUACIONS" />
+        <q-route-tab to="/pitagoras" label="PITAGORAS" />
+      </q-tabs>
+    </q-drawer>
+    <q-drawer v-if="divActivo === 'jocs'" show-if-above v-model="isLeftDrawerOpen" side="left" bordered>
+      <q-tabs vertical>
+        <q-route-tab to="/ocaMatematica" label="Oca Matematica" />
+        <q-route-tab to="/buscaminas" label="BUSCAMINAS" />
+      </q-tabs>
+    </q-drawer>
+    <q-drawer v-if="divActivo === 'online'" show-if-above v-model="isLeftDrawerOpen" side="left" bordered>
+      <q-tabs vertical>
+        <q-route-tab to="/sumaOffline" label="SUMA" />
+        <q-route-tab to="/restaOffline" label="RESTA" />
+        <q-route-tab to="/multiplicacionOffline" label="MULTIPLICACION" />
+        <q-route-tab to="/divisionOffline" label="DIVISION" />
+      </q-tabs>
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
 
+    <q-footer elevated style="height: calc(50px + 5px);">
+      <!-- Contenido adicional del footer -->
+      <div class="text-center py-2">
+        <!-- Símbolo de copyright en ASCII y enlace a institutpedralbes.cat -->
+        <p>&#169; 2024 GoMath. Todos los derechos reservados. <a href="https://www.institutpedralbes.cat"
+            target="_blank" class="text-white">institutpedralbes.cat</a></p>
+
+        <!-- Agradecimientos o disclaimers -->
+        <p style="font-size: 10px; color: #F7FFF7;">
+          La información contenida en esta aplicación es solo para fines educativos. No nos responsabilizamos por su uso
+          inapropiado.
+        </p>
+
+      </div>
+    </q-footer>
   </q-layout>
+
+
 </template>
