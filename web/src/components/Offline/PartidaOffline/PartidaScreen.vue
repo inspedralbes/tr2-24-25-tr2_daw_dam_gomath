69d1299db564b663d7d0e71ff35811e03cffde82
AyoubBoudhafr <a23mohbounij@inspedralbes.cat>
2024-12-05 10:36:32 +0100

    fetch de comunication manager creado

--- web/src/components/Offline/PartidaOffline/PartidaScreen.vue
+++ web/src/components/Offline/PartidaOffline/PartidaScreen.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="q-pa-md botones-container">
+    <!-- Dropdown para el número de preguntas -->
     <q-btn-dropdown color="primary" label="NUMERO DE PREGUNTAS">
       <q-list>
-        <q-item clickable v-close-popup @click="onItemClick">
+        <q-item clickable v-close-popup @click="onDropdownChange">
           <q-item-section>
             <q-item-label>10</q-item-label>
           </q-item-section>
         </q-item>
-
-        <q-item clickable v-close-popup @click="onItemClick">
+        <q-item clickable v-close-popup @click="onDropdownChange">
           <q-item-section>
             <q-item-label>20</q-item-label>
           </q-item-section>
         </q-item>
-
-        <q-item clickable v-close-popup @click="onItemClick">
+        <q-item clickable v-close-popup @click="onDropdownChange">
           <q-item-section>
             <q-item-label>30</q-item-label>
           </q-item-section>
@@ -22,21 +21,20 @@
       </q-list>
     </q-btn-dropdown>
 
+    <!-- Dropdown para el cronómetro -->
     <q-btn-dropdown color="primary" label="CRONO">
       <q-list>
-        <q-item clickable v-close-popup @click="onItemClick">
+        <q-item clickable v-close-popup @click="onDropdownChange">
           <q-item-section>
             <q-item-label>30seg</q-item-label>
           </q-item-section>
         </q-item>
-
-        <q-item clickable v-close-popup @click="onItemClick">
+        <q-item clickable v-close-popup @click="onDropdownChange">
           <q-item-section>
             <q-item-label>1min</q-item-label>
           </q-item-section>
         </q-item>
-
-        <q-item clickable v-close-popup @click="onItemClick">
+        <q-item clickable v-close-popup @click="onDropdownChange">
           <q-item-section>
             <q-item-label>3min</q-item-label>
           </q-item-section>
@@ -44,21 +42,20 @@
       </q-list>
     </q-btn-dropdown>
 
+    <!-- Dropdown para los fallos -->
     <q-btn-dropdown color="primary" label="HASTA QUE FALLES">
       <q-list>
-        <q-item clickable v-close-popup @click="onItemClick">
+        <q-item clickable v-close-popup @click="onDropdownChange">
           <q-item-section>
             <q-item-label>1 Fallo</q-item-label>
           </q-item-section>
         </q-item>
-
-        <q-item clickable v-close-popup @click="onItemClick">
+        <q-item clickable v-close-popup @click="onDropdownChange">
           <q-item-section>
             <q-item-label>3 Fallos</q-item-label>
           </q-item-section>
         </q-item>
-
-        <q-item clickable v-close-popup @click="onItemClick">
+        <q-item clickable v-close-popup @click="onDropdownChange">
           <q-item-section>
             <q-item-label>5 Fallos</q-item-label>
           </q-item-section>
@@ -69,15 +66,27 @@
 </template>
 
 <script>
+import { useOperationsStore } from '@/stores/comunicationManager';  
+
 export default {
-  setup () {
+  data() {
     return {
-      onItemClick () {
-        // console.log('Clicked on an Item')
-      }
-    }
-  }
-}
+      selectedQuestions: null,
+      selectedCrono: null,
+      selectedFails: null
+    };
+  },
+  methods: {
+    onDropdownChange() {
+      this.loadOperations();
+    },
+
+    async loadOperations() {
+      const operationsStore = useOperationsStore();  
+      await operationsStore.fetchOperations();  
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -93,7 +102,6 @@ export default {
   padding: 10px 20px;
   height: 50px; 
 }
-
 
 .q-item-label {
   font-weight: bold;
