6fc9bd7c3dd5b3c9932e5e24de315fd5ecc31804
GerardArias <a23gerarimar@inspedralbes.cat>
2024-12-09 09:37:18 +0100

    a

--- back/laravel/routes/web.php
+++ back/laravel/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OperacionController;
+use App\Http\Controllers\ClaseController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,5 +14,19 @@ Route::get('/adminer', function () {
 
 Route::get('/operacion', [OperacionController::class, 'index'])->name('operacions');
 Route::delete('/operacion/{id}', [OperacionController::class, 'destroy'])->name('operacions.destroy');
-
 Route::resource('operacions', OperacionController::class);
+
+// Mostrar todas las clases
+Route::get('/clases', [ClaseController::class, 'index'])->name('clases.index');
+// Mostrar formulario para crear una nueva clase
+Route::get('/clases/create', [ClaseController::class, 'create'])->name('clases.create');
+// Crear una nueva clase
+Route::post('/clases', [ClaseController::class, 'store'])->name('clases.store');
+// Mostrar una clase específica
+Route::get('/clases/{id}', [ClaseController::class, 'show'])->name('clases.show');
+// Mostrar el formulario de edición de una clase
+Route::get('/clases/{id}/edit', [ClaseController::class, 'edit'])->name('clases.edit');
+// Actualizar una clase
+Route::put('/clases/{id}', [ClaseController::class, 'update'])->name('clases.update');
+// Borrar una clase
+Route::delete('/clases/{id}', [ClaseController::class, 'destroy'])->name('clases.destroy');
