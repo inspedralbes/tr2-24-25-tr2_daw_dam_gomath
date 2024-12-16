dd54d722a520294e1a9f52d5e865dae7e5fe6d25
Andy <a23andturmat@inspedralbes.cat>
2024-12-16 10:23:06 +0100

    canvis crud login

--- back/laravel/app/Http/Controllers/LoginRegisterController.php
+++ back/laravel/app/Http/Controllers/LoginRegisterController.php
@@ -47,34 +47,5 @@ class LoginRegisterController extends Controller
         return redirect()->route('home');
     }
 
-    public function register(Request $request)
-    {
-        $data = $request->validate([
-            'username' => 'required',
-            'email' => 'required|email',
-            'password' => 'required'
-        ], [
-            'username.required' => 'El campo nombre es obligatorio',
-            'email.required' => 'El campo email es obligatorio',
-            'email.email' => 'El campo email debe ser una dirección válida',
-            'password.required' => 'El campo password es obligatorio'
-        ]);
-
-        try {
-            $user = new User();
-            $user->name = $data['username'];
-            $user->email = $data['email'];
-            $user->password = bcrypt($data['password']);
-            $user->rol = 'user';
-            $user->save();
-
-            Auth::login($user);
-
-            return redirect()->route('welcome');
-
-        } catch (\Exception $e) {
-            return back()->withErrors(['error' => 'Hubo un problema al registrar el usuario: ' . $e->getMessage()])->withInput();
-        }
-    }
 
 }
--- back/laravel/resources/views/auth/login_register.blade.php
+++ back/laravel/resources/views/auth/login_register.blade.php
@@ -31,52 +31,16 @@
                         <button type="submit" class="btn btn-primary w-100 rounded-pill fw-bold">Iniciar Sessió</button>
                     </form>
 
-                    <div class="d-flex align-items-center justify-content-center pb-4 mt-3">
-                        <p class="mb-0 me-2 text-secondary">No tens un compte?</p>
-                        <button class="btn btn-outline-primary rounded-pill" onclick="toggleForms()">Crear nova</button>
-                    </div>
                 </div>
             </div>
         </div>
 
-        <!-- Formulari Registre -->
-        <div class="col-md-5 ms-4" id="registerForm" style="display: none;">
-            <div class="card shadow-lg border-0">
-                <div class="card-body">
-                    <h2 class="text-center text-primary fw-bold mb-4">Registrar-se</h2>
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-                        <div class="mb-3">
-                            <label for="username" class="form-label text-secondary">Nom d'Usuari</label>
-                            <input type="text" class="form-control rounded-pill shadow-sm" id="username" name="username" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="register-email" class="form-label text-secondary">Correu Electrònic</label>
-                            <input type="email" class="form-control rounded-pill shadow-sm" id="register-email" name="email" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="register-password" class="form-label text-secondary">Contrasenya</label>
-                            <input type="password" class="form-control rounded-pill shadow-sm" id="register-password" name="password" required>
-                        </div>
-                        <button type="submit" class="btn btn-primary w-100 rounded-pill fw-bold">Registrar-se</button>
-                    </form>
-
-                    <div class="d-flex align-items-center justify-content-center pb-4 mt-3">
-                        <p class="mb-0 me-2 text-secondary">Ja tens un compte?</p>
-                        <button class="btn btn-outline-primary rounded-pill" onclick="toggleForms()">Iniciar Sessió</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
 
 @section('page-script')
 <script>
     function toggleForms() {
         var loginForm = document.getElementById('loginForm');
-        var registerForm = document.getElementById('registerForm');
 
         if (loginForm.style.display === "none") {
             loginForm.style.display = "block";
