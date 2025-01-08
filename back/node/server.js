55f3bfcc9c34f892c2aaa3562c3bd5e097e557f2
a18marcastru <a18marcastru@inspedralbes.cat>
2025-01-08 09:04:23 +0100

    Canvi de port

--- back/node/server.js
+++ back/node/server.js
@@ -114,7 +114,7 @@ io.on('connection', (socket) => {
 });
 
 // Iniciar el servidor
-const PORT = process.env.PORT || 3011;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Servidor en funcionament a http://localhost:${PORT}`);
 });
